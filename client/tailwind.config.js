fde897f16a372eb43b5d31b9e5ad6746e4b0f270
jashezen <84038649+jashezan@users.noreply.github.com>
2023-10-14 12:55:58 +0600

    Initialized the Application

---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      Roboto: ["Roboto", "sans-serif"],
+      Popins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        xs: "320px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        xxl: "1536px",
+      },
+    },
+  },
+  plugins: [],
+};
