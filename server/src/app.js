064e691577af1a4b4659acba2621a5e729c85a3b
jashezen <84038649+jashezan@users.noreply.github.com>
2023-10-20 19:16:33 +0600

    Removed express-fileupload from 'app.js' because Multer library will be used instead

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const fileUpload = require("express-fileupload");
 
 const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
@@ -8,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(fileUpload());
+
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
