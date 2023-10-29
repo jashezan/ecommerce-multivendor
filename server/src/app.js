79af53620cf954ce4bda075f5c5655acfce6ad29
jashezen <84038649+jashezan@users.noreply.github.com>
2023-10-30 00:38:14 +0600

    Half Configured User Controller

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 
+const userController = require("./controllers/user");
+
 const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
+app.use("/", express.static("public"))
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "server/config/.env" });
 }
+
+app.use("/api/v1/user", userController);
 
 // its for error handling
 
--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,9 +1,36 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const router = express.Router();
-const {upload} = require("../multer");
+const { upload } = require("../multer");
+const User = require("../models/user");
+const ErrorHandler = require("../utils/ErrorHandler");
 
-router.post('/create-user', upload.single("file"), async(req, res)=>{
-  const {name, email, password} = req.body;
-  
-})
+router.post("/create-user", upload.single("file"), async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return next(new ErrorHandler(400, "User already exists"));
+  }
+
+  const filename = req.file.filename;
+  const fileUrl = path.join(__dirname, `../../public/uploads/${filename}`);
+
+  const user = User.create({
+    name,
+    email,
+    password,
+    avatar: fileUrl,
+  });
+
+  if (!user) {
+    return next(new ErrorHandler(400, "User not created"));
+  }
+
+  res.status(201).json({
+    success: true,
+    message: "User created successfully",
+    user,
+  });
+});
+
+module.exports = router;
